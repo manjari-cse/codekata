931e39916e899be15db594f7c7e09310f41226e1
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-02-03 00:10:02 +0530

    Create sort and min.c

---
+++ beginner/set11/sort and min.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	long int i,a[10],t,j;
+	for(i=0;i<10;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<10;i++)
+	{
+	for(j=1;j<10;j++)
+	{if(a[i]>a[j])
+	{t=a[i];
+	a[i]=a[j];
+	a[j]=t;
+	}
+	}
+}
+	printf("%d",a[0]);
+}
