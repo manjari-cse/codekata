dbfc4950e6a0031ca9c4a6c8f5ecd54d5e40fc4a
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-02-13 16:39:36 +0530

    Create largest no of char15.c

---
+++ player/set2/largest no of char15.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+	char a[20];
+	int i,j,count,max,count1=0;
+	gets(a);
+	for(i=0;a[i]!='\0';i++)
+	{
+	count=0;
+	for(j=i;a[j]!='\0';j++)
+	{if(a[i]==a[j])
+	count++;
+	}
+	if(count>max)
+	max=count;
+	}
+	printf("%d",max);
+	for(i=0;a[i]!='\0';i++)
+	{
+		count1=0;
+		for(j=i;a[j]!='\0';j++)
+		{if(a[i]==a[j])
+		count1++;
+		}
+	if(count1==max)
+	{
+	printf("%c",a[i]);
+break;
+}
+}
+}
