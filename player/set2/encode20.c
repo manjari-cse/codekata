7302413d33423b8984f065553be7bfc8dc52b884
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-02-15 16:35:40 +0530

    Update encode20.c

--- player/set2/encode20.c
+++ player/set2/encode20.c
@@ -6,6 +6,13 @@ void main()
 	scanf("%s",a);
 	for(i=0;a[i]!='\0';i++)
 	{
+ 		if(a[i]=='X')
+ 			b[i]='A';
+ 		else if(a[i]=='Y')
+ 			b[i]='B';
+ 		else if(a[i]=='Z')
+ 			 b[i]='C';
+ 		else
 		b[i]=a[i]+3;
 	}
 	printf("%s",b);
