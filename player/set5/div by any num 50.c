139644e9797e061d04f15645fffc0b7952094315
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-03-08 15:59:06 +0530

    Create div by any num 50.c

---
+++ player/set5/div by any num 50.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{long int i,n,count=0;
+	scanf("%d",&n);
+	for(i=2;i<n;i++)
+	{
+		if(n%i==0)
+		count++;
+	}
+	if(count==0)
+	printf("no");
+	else
+	printf("yes");
+}
