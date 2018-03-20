649eccdc94ec8e5fe9826b67628435106ba943b4
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-03-20 16:38:12 +0530

    Create odd array or even array 76.c

---
+++ player/set8/odd array or even array 76.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main()
+{
+	int N,a[10000],i,count=0,j;
+	scanf("%d",&N);
+	for(i=0;i<N;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<3;i++)
+	{
+		if(a[i]%2==0)
+		count++;
+	}
+	if(count>1)
+	{
+	for(i=0;i<N-1;i++)
+	{
+		if(a[i]%2!=0)
+		{
+		printf("%d",a[i]);
+		break;
+		}
+	}
+}
+else
+{
+		for(i=0;i<N-1;i++)
+	{
+		if(a[i]%2==0)
+		{
+		printf("%d",a[i]);
+		break;
+		}
+	}
+}
+}
