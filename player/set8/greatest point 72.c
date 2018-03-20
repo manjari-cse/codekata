af45f4774fde8e86444d4a2cc042e8eafae0faa2
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-03-20 16:35:21 +0530

    Create greatest point 72.c

---
+++ player/set8/greatest point 72.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int N,a[10000],i;
+	scanf("%d",&N);
+	for(i=0;i<N;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<N;i++)
+	{
+		if(a[i+1]<a[i])
+		break;
+	}
+	printf("%d",a[i]);
+}
